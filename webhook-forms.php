7a50971bca1e5bc13925717c45647f40262e4e8f
Mounir Qarchane <mqarchane@users.noreply.github.com>
2017-04-11 10:25:49 -0500

    Update webhook-forms.php

--- webhook-forms.php
+++ webhook-forms.php
@@ -21,45 +21,41 @@ $leadgen_id = $data['entry'][0]['changes'][0]['value']['leadgen_id']; // extract
 
 // check leadgen ID before calling Facebook API
 if($leadgen_id){
-	$ch = curl_init();
-
-	$url = "https://graph.facebook.com/v2.8/".$leadgen_id;
-	$url_query = "access_token=".$fb_access_token; 
-
-	$url_final = $url.'?'.$url_query;
-	curl_setopt($ch, CURLOPT_URL, $url_final);
-	curl_setopt($ch, CURLOPT_HTTPGET, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$response = curl_exec($ch);
+  $ch = curl_init();
+  $url = "https://graph.facebook.com/v2.8/".$leadgen_id;
+  $url_query = "access_token=".$fb_access_token; 
+  $url_final = $url.'?'.$url_query;
+  curl_setopt($ch, CURLOPT_URL, $url_final);
+  curl_setopt($ch, CURLOPT_HTTPGET, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $response = curl_exec($ch);
   //if (curl_errno($ch)) { 
   //    print curl_error($ch); 
   //} 
   curl_close ($ch);
-	$data = json_decode($response, true);
-	//var_dump($data);
-	$lead_first = $data['field_data'][0][values][0];
-	$lead_last = $data['field_data'][1][values][0];
-	$lead_email = $data['field_data'][2][values][0];
+  $data = json_decode($response, true);
+  //var_dump($data);
+  $lead_first = $data['field_data'][0][values][0];
+  $lead_last = $data['field_data'][1][values][0];
+  $lead_email = $data['field_data'][2][values][0];
 
 } 
 
 // check if we have a valid lead before calling Mautic Api
 if($lead_email){
 
-	//Create a new Contact
-
-	$data =array(); 
-
-	$data['first_name'] = $lead_first;
-	$data['last_name'] = $lead_last;
-	$data['email'] = $lead_email;
+  //Create a new Contact
+  $data =array(); 
+  $data['first_name'] = $lead_first;
+  $data['last_name'] = $lead_last;
+  $data['email'] = $lead_email;
   $data['submit'] = "";
   $data['formId'] = 4;
-
-
   $formId = $data['formId'];
+	
   $data = array('mauticform' => $data);
-    // Change [path-to-mautic] to URL where your Mautic is
+  
+  // Change [path-to-mautic] to URL where your Mautic is
   $formUrl =  '[path-to-mautic]/form/submit?formId=' . $formId;
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $formUrl);
