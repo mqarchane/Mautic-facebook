a2cad4bd54418595fbd387266367412992e28490
Mounir Qarchane <mqarchane@users.noreply.github.com>
2017-04-22 18:10:47 -0500

    Update webhook-forms.php

--- webhook-forms.php
+++ webhook-forms.php
@@ -1,25 +1,18 @@
 <?php
-
 // facebook variables 
 $challenge = isset($_REQUEST['hub_challenge']) ? $_REQUEST['hub_challenge'] : '';
 $verify_token = isset($_REQUEST['hub_verify_token']) ? $_REQUEST['hub_verify_token'] : ''; 
-
-$fb_access_token = ""; // you have to subscribe to the page that has the form to generate an Access Token
+$fb_access_token = "";
 
 // this is used to subscribe to facebook webhook
-if ($verify_token === 'abc133') { // 
+if ($verify_token === "mautic01") { // 
   echo $challenge;
 }
-
-//this is for testing
-//$string = '{"entry":[{"changes":[{"field":"leadgen","value":{"ad_id":0,"form_id":71742323768434126,"leadgen_id":7175053384232331969,"created_time":1491871976,"page_id":2100872676805012,"adgroup_id":0}}],"id":"2100872676805012","time":1491871977}],"object":"page"}	';
-//$data = json_decode($string, true);
 
 // Process retrieved data from facebook webhook
 $data = json_decode(file_get_contents("php://input"),true);
 $leadgen_id = $data['entry'][0]['changes'][0]['value']['leadgen_id']; // extract leadgen ID
 
-// check leadgen ID before calling Facebook API
 if($leadgen_id){
   $ch = curl_init();
   $url = "https://graph.facebook.com/v2.8/".$leadgen_id;
@@ -29,30 +22,23 @@ if($leadgen_id){
   curl_setopt($ch, CURLOPT_HTTPGET, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $response = curl_exec($ch);
-  //if (curl_errno($ch)) { 
-  //    print curl_error($ch); 
-  //} 
   curl_close ($ch);
   $data = json_decode($response, true);
-  //var_dump($data);
-  $lead_first = $data['field_data'][0][values][0];
+  $lead_first = $data['field_data'][2][values][0];
   $lead_last = $data['field_data'][1][values][0];
-  $lead_email = $data['field_data'][2][values][0];
-
+  $lead_email = $data['field_data'][0][values][0];
 } 
 
 // check if we have a valid lead before calling Mautic Api
 if($lead_email){
-
   //Create a new Contact
   $data =array(); 
   $data['first_name'] = $lead_first;
   $data['last_name'] = $lead_last;
   $data['email'] = $lead_email;
   $data['submit'] = "";
-  $data['formId'] = 4;
+  $data['formId'] = 3; // change this to the form ID that you created in your Mautic
   $formId = $data['formId'];
-	
   $data = array('mauticform' => $data);
   
   // Change [path-to-mautic] to URL where your Mautic is
@@ -63,15 +49,8 @@ if($lead_email){
   curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response = curl_exec($ch);
-  //if (curl_errno($ch)) { 
-  //    print curl_error($ch); 
-  //} 
   curl_close($ch);
   return $response;
-
   echo "Contact Added!";
-  //print_r($response); 
-
 } // end valid lead check
-
 ?>
